a02476dc7a63af1966079121b38164387904edab
Abdullah Sayed <absayed824@gmail.com>
2025-10-24 16:19:10 +0600

    Add files via upload

---
+++ if-else3.1.c
@@ -0,0 +1,21 @@
+//WAP to swap two numbers using third variable
+
+#include<stdio.h>
+int main(){
+
+    int a,b,c;
+    printf("Enter 1st number : ");
+    scanf("%d",&a);
+    printf("Enter 2nd number : ");
+    scanf("%d",&b);
+
+    c=a;
+    a=b;
+    b=c;
+
+    printf("1st number after swap = %d\n",a);
+
+    printf("2nd number after swap = %d\n",b);
+
+    return 0;
+}
