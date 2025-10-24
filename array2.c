200948f9b47d2fe2f52853d1274040ad5bbeb36b
Abdullah Sayed <absayed824@gmail.com>
2025-10-24 18:05:15 +0600

    Add files via upload

---
+++ array2.c
@@ -0,0 +1,26 @@
+//WAP to print negative elements
+
+#include<stdio.h>
+int main(){
+
+    int n;
+    printf("Enter size if array : ");
+    scanf("%d",&n);
+
+    int arr[n],i;
+    printf("Enter elements of Array : ");
+
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("All negative elements are : ");
+
+    for(i=0;i<n;i++){
+        if(arr[i]<0){
+            printf("%d ",arr[i]);
+        }
+
+    }
+
+    return 0;
+}
