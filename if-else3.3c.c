4222b91911616984f7bc458035f500a63867eaa7
Abdullah Sayed <absayed824@gmail.com>
2025-10-24 16:26:39 +0600

    Add files via upload

---
+++ if-else3.3c.c
@@ -0,0 +1,21 @@
+//WAP to swap two numbers using arithmetic operator
+
+#include<stdio.h>
+int main(){
+
+    int a,b;
+    printf("Enter 1st number : ");
+    scanf("%d",&a);
+    printf("Enter 2nd number : ");
+    scanf("%d",&b);
+
+    a = a+b;
+    b = a-b;
+    a = a-b;
+
+    printf("1st number after swap = %d\n",a);
+
+    printf("2nd number after swap = %d\n",b);
+
+    return 0;
+}
