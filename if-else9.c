6d523b470312572d3ac5e14e0e59b92b5b2fb440
Abdullah Sayed <absayed824@gmail.com>
2025-10-24 17:44:18 +0600

    Add files via upload

---
+++ if-else9.c
@@ -0,0 +1,35 @@
+//WAP to print name of the day using switch
+
+#include<stdio.h>
+int main(){
+
+    int n;
+    printf("Enter day number(1-7) : ");
+    scanf("%d",&n);
+    switch(n){
+        case 1:
+            printf("Saturday");
+            break;
+        case 2:
+            printf("Sunday");
+            break;
+        case 3:
+            printf("Monday");
+            break;
+        case 4:
+            printf("Tuesday");
+            break;
+        case 5:
+            printf("Wednesday");
+            break;
+        case 6:
+            printf("Thursday");
+            break;
+        case 7:
+            printf("Friday");
+            break;
+
+    }
+
+    return 0;
+}
