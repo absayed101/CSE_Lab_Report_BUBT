c201204e126c1959f0814c59cfd8e15177a14d63
Abdullah Sayed <absayed824@gmail.com>
2025-10-24 18:12:52 +0600

    Add files via upload

---
+++ array3.c
@@ -0,0 +1,22 @@
+//WAP sum of all elements
+
+#include<stdio.h>
+int main(){
+
+    int n;
+    printf("Enter size of array : ");
+    scanf("%d",&n);
+
+    int arr[n],i,sum=0;
+    printf("Enter elements of Array : ");
+
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    for(i=0;i<n;i++){
+        sum += arr[i];
+    }
+    printf("Sum of all elements is : %d",sum);
+    return 0;
+}
