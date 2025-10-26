52400e973003cbe0a02dd3450920f50884d8da4f
Abdullah Sayed <absayed824@gmail.com>
2025-10-26 11:08:46 +0600

    Add files via upload

---
+++ if-else10.c
@@ -0,0 +1,16 @@
+//WAP to even or odd
+
+#include<stdio.h>
+int main(){
+
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+
+    if(n%2==0){
+        printf("The number '%d' is even",n);
+    }else {
+       printf("The number '%d' is odd",n);
+    }
+    return 0;
+}5
